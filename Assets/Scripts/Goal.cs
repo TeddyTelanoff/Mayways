5031819f3912528909f4e91567913016daa03c0f
Treidex <treidexsemextetry@gmail.com>
2021-03-22 17:01:55 -0700

    Graphics!

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 m_Direction;
+	[SerializeField]
+	private float m_Speed;
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + m_Direction * m_Speed * Time.deltaTime);
+    }
+}
